6aee5ec303836046b57779d28827ab56dffbbb0b
hasomy <61577384+hasomy@users.noreply.github.com>
2026-05-16 15:06:00 +0300

    Add files via upload

---
+++ configs/config.js
@@ -0,0 +1,52 @@
+﻿Config({
+    "network-name": "  الانصار نت",
+    "service-number": "770977933",
+    "popup-adv-time": 5,
+    "popup-adv-img-type": true,
+    "popup-adv-repeat-after": 120,
+    "login-type": "User",
+    "news-line": " مرحبا بكم في شبكة الانصار نت  ",
+    "input-type": "tel", 
+    "input-autocomplete": "on",
+    "speed-trychange": 3,//عدد مرات ناساح التغيير
+    "speed-trychange-timeout": 3,// مدة فترة  حضر التغيير بالدقائق
+    "input-rm-white-spaces": 0,
+    "input-to-lower": 0,
+    "input-to-upper": 0,
+    "input-to-arabic-numbers": 1,
+    "input-only-numbers": 0,
+    "input-no-numbers": 0,
+    "input-only-alphanumeric": 0,
+    "input-to-tel-type-when": 0,
+    "enable-hot-cookie": 1,
+    "enable-hot-blocker": 1,
+    "clear-router-cookie": 1,
+    "clear-hot-cookie": 1,
+    "block-time": 1,
+    "try-count": 20,
+    "warn-when": 10,
+    "warn-message": "تحذير !! عدد محاولاتك الخاطئة اصبح {{tryCounter}} محاولات, عدد المحاولات المسموح بها هي {{tryCount}} محاولات فقط, عدد محاولاتك المتبقية {{restTryCount}} محاولات, سيتم حظرك لمدة {{blockTime}} دقائق اذا تجاوزت العدد المسموح للمحاولات",
+    "price-button": true,
+    "sell-point-button": true,
+    "app-store-status-button": false,
+    "show-date-field": true,
+    "loan-button": true,
+    "login-speeds-mode": true,
+    "redirect-to-esterahah": "",
+    "redirect-to-mobasher": "live/index.html",
+    "app-store-base-url": "",
+    "app-store-base-url-ext": "",
+    "profiles": [],
+    "sell-points": []
+})
+
+
+
+
+
+
+
+
+
+
+
